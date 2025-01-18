23631812cc3a9adba5c21ae27dc71c9771c7100b
x-tropy <buweiliao@gmail.com>
2025-01-18 20:41:35 +0100

    fix: add missing component

---
+++ src/components/UserPosts.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2>Posts</h2>
+  <p>{{ $route.params.id }}</p>
+  <p>{{ $route.params.id }}</p>
+  <p>{{ $route.params.id }}</p>
+  <p>{{ $route.params.id }}</p>
+  <p>{{ $route.params.id }}</p>
+</template>
+
+<style scoped>
+
+</style>
