125e71c75a9a4b786b99975b77d31698a112d668
x-tropy <buweiliao@gmail.com>
2025-01-22 17:07:17 +0100

    new Examples: Button, ResizablePanel

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faGithub, faXTwitter} from "@fortawesome/free-brands-svg-icons";
-import {Search, ChevronDown,GitBranch, Database, SquareTerminal, Bug, Inbox, Send, X, Map, Trash2, Bell, UserRoundPlus} from "lucide-vue-next"
-import {useWindowSize} from '@vueuse/core'
-import {computed, ref} from 'vue'
-
-const {width, height} = useWindowSize()
-
+import {ref} from 'vue'
+import Header from 'Components/navigation/Header.vue'
+import ResizablePanelExample from "Components/container/ResizablePanelExample.vue";
 const textMenus = [
   {
     text: 'Components',
@@ -29,7 +25,6 @@ const textMenus = [
     url: '/roadmap',
   }
 ]
-
 const iconMenus = [
   {
     icon: faGithub,
@@ -42,72 +37,17 @@ const iconMenus = [
 ]
 
 const maskOn = ref(false)
-const panelOn = computed(() => maskOn.value)
-const togglePanel = () => {
+const toggle = () => {
   maskOn.value = !maskOn.value
 }
+
 </script>
 
 
 <template>
-
-  <header class="navbar">
-    <RouterLink to="/" class="logo"></RouterLink>
-    <nav class="menus-wrap">
-      <button
-        class="menu-item menu-text sm:hidden"
-        @click="togglePanel"
-      >Menu items
-        <ChevronDown class="ml-1 h-3 w-3"/>
-      </button>
-      <ul class="menus-wrap" :class="{'panel-drop': width < 640, '!hidden': !panelOn && width < 640}">
-        <li
-          class="h-full"
-          v-for="el in textMenus"
-        >
-          <a
-            v-if="el?.external"
-            :href="el.url"
-            class="menu-item menu-text"
-            target="_blank">
-            {{ el.text }}
-            <span class="external">↗</span>
-          </a>
-          <RouterLink
-            v-else
-            :to="el.url"
-            :key="el.text"
-            class="menu-item menu-text"
-            :class="{active: $route.fullPath.endsWith(el.url)}"
-          >{{ el.text }}
-            <span class="indicator"></span>
-          </RouterLink>
-        </li>
-      </ul>
-      <span class="divider-v lg-show"></span>
-      <button class="menu-item">
-        <Search class="menu-icon"/>
-        <span class="lg-show ml-1 mr-6 text-sm text-gray-400">Search...</span>
-        <kbd class="lg-show text-xs font-semibold !text-gray-400 rounded-sm px-1 py-0.5 btn-outline">Ctrl+K</kbd>
-      </button>
-      <span class="divider-v lg-show"></span>
-      <a v-for="el in iconMenus" target="_blank" class="menu-item" :href="el.url">
-        <font-awesome-icon class="menu-icon" :icon="el.icon"/>
-      </a>
-      <div class="menu-item" v-tippy="{ content: 'NotificationNotificationNotificationNotification', offset:[0, 0], trigger:'mouseenter click', animation: 'shift-away', interactive: true, placement: 'bottom' }">
-        <button class="btn-icon btn-ghost">
-          <Bell/>
-          <span class="dot"></span>
-        </button>
-      </div>
-    </nav>
-    <!--    <div class="absolute bg-white w-full left-0 top-14">abc</div>-->
-  </header>
-
-
-  <main class="wide">
-
-    <RouterView/>
+  <Header @toggleMenusPanel="toggle" :textMenus="textMenus" :iconMenus="iconMenus" :maskOn="maskOn"/>
+  <main class="wide my-10">
+    <ResizablePanelExample/>
   </main>
-  <div class="mask" @click="maskOn = !maskOn" :class="{'hidden': !maskOn}"></div>
+  <div class="mask" @click="maskOn = !maskOn" :class="{'opacity-0 invisible': !maskOn}"></div>
 </template>
---
+++ src/components/Components/container/ResizablePanelExample.vue
@@ -0,0 +1,63 @@
+<script setup>
+import ResizablePanel from "Components/container/ResizablePanel.vue";
+
+</script>
+
+<template>
+  <div class="h-[500px] flex flex-col box">
+    <!--      min-h-0 overflow-hidden make sure the ResizablePanel's container remain unaffected by its children-->
+    <div class="flex flex-1 min-h-0 overflow-hidden">
+      <ResizablePanel panelId="1" class="bg-white">
+        <div class="select-none h-full overflow-auto">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+          aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+          dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+          sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+          magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+          suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+          nulla pariatur? Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+          consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+          ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+          labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
+          ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure
+          reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
+          fugiat quo voluptas nulla pariatur?
+        </div>
+      </ResizablePanel>
+      <div class="bg-slate-200 text-cyan-500 select-none font-mono flex-1 flex items-center justify-center"><- resize
+        them
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;↓
+      </div>
+    </div>
+
+    <ResizablePanel class="bg-white border-t border-gray-200" panelId="2" direction="vertical">
+      <div class="select-none h-full overflow-auto">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+        amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+        aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+        laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
+        voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+        pariatur? Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+        magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
+        dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+        suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+        ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+        pariatur?
+      </div>
+    </ResizablePanel>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
