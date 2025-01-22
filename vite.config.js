15bc220e7ef9fd9ffebd1129c892d61981ed3130
x-tropy <buweiliao@gmail.com>
2025-01-22 17:04:48 +0100

    simplify import path for: Components, Blocks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      'Components': path.resolve(__dirname, './src/components/Components'),
+      'Blocks': path.resolve(__dirname, './src/components/Blocks'),
+    },
+  },
 })
